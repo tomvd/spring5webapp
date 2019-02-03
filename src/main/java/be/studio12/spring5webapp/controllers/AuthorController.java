1a16b4561b9cf06cb47bf16c4c74a010ba8d8dd0
Tom Van Dyck <tomvdtom@gmail.com>
2019-02-03 12:58:56 +0100

    added views

---
+++ src/main/java/be/studio12/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,26 @@
+package be.studio12.spring5webapp.controllers;
+
+import be.studio12.spring5webapp.repositories.AuthorRepository;
+import be.studio12.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model){
+
+        model.addAttribute("authors", authorRepository.findAll());
+
+        return "authors";
+
+    }
+}
